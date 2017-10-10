116c4a39ce98f25d79af87bb0d08f2bd0dbcebd9
Steve Geluso <stevegeluso@gmail.com>
2017-10-10 15:47:18 -0700

    detecting [enter] keypress when entering username

--- app/src/main/java/com/example/moonmayor/whatswhatsapp/LoginActivity.java
+++ app/src/main/java/com/example/moonmayor/whatswhatsapp/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.moonmayor.whatswhatsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by moonmayor on 10/4/17.
@@ -29,13 +32,26 @@ public class LoginActivity extends AppCompatActivity {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = mUsernameInput.getText().toString();
-                initiateLogin(username);
+                initiateLogin();
+            }
+        });
+
+        mUsernameInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                Log.d("PRESS", "" + keyEvent.getKeyCode());
+                // detect if the user presses [enter]
+                if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    initiateLogin();
+                    return true;
+                }
+                return false;
             }
         });
     }
 
-    private void initiateLogin(String username) {
+    private void initiateLogin() {
+        String username = mUsernameInput.getText().toString();
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         intent.putExtra("username", username);
         startActivity(intent);
